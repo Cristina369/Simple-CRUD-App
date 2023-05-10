216b5505bc198b97f775a9f25559cf94c64ba702
Cristina <cristy.wolf666@gmail.com>
2023-05-10 15:27:57 +0300

    json server setup

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,24 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 function Home() {
-  return <div>Home</div>;
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    axios
+      .get("http://localhost:3000/users")
+      .then((res) => setData(res.data))
+      .catch((err) => console.log(err));
+  }, []);
+  return (
+    <div>
+      <h1>List of users</h1>
+      <div>
+        {data.map((datas, index) => {
+          <h1 key={index}>{datas.name}</h1>;
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default Home;
